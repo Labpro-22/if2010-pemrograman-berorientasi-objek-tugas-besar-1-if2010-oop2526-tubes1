0be63964d154cdccc18351cd69c5ed3a7b9fe7fe
argha <164616992+arghaarham@users.noreply.github.com>
2026-04-12 11:33:44 +0700

    feat: enums, save/load, and formatter headers

--- include/core/Exceptions.hpp
+++ include/core/Exceptions.hpp
@@ -2,15 +2,53 @@
 #define EXCEPTIONS_HPP
 
 #include <exception>
+#include <string>
+#include "Enums.hpp"
 
-class NotEnoughMoneyException : public std::exception {
+class GameException : public std::exception{
+private:
+    GameErrorCode errorCode;
+    std::string errorMessage;
+
 public:
-    const char* what() const noexcept override;
+    GameException(GameErrorCode errorCode, const std::string& errorMessage) : errorCode(errorCode), errorMessage(errorMessage){}
+
+    GameErrorCode getErrorCode() const{
+        return errorCode;
+    }
+
+    std::string getErrorMessage() const{
+        return errorMessage;
+    }
+
+    const char* what() const noexcept override{
+        return errorMessage.c_str();
+    }
 };
 
-class MaxCardLimitException : public std::exception {
+class NotEnoughMoneyException : public GameException{
 public:
-    const char* what() const noexcept override;
+    NotEnoughMoneyException(int have, int need, const std::string& context) : GameException( GameErrorCode::NOT_ENOUGH_MONEY, "Uang tidak cukup untuk: " + context + ". Punya: " + std::to_string(have) + ", Butuh: " + std::to_string(need)){}
+};
+
+class MaxCardLimitException : public GameException{
+public:
+    MaxCardLimitException() : GameException(GameErrorCode::MAX_CARD_LIMIT, "Slot untuk kartu sudah limit! Maksimal 3"){}
+};
+
+class InvalidCommandException : public GameException{
+public:
+    InvalidCommandException(const std::string command) : GameException(GameErrorCode::INVALID_COMMAND, "Command " + command + " tidak dikenali!"){}
+};
+
+class InvalidPropertyException : public GameException{
+public:
+    InvalidPropertyException(const std::string kode) : GameException(GameErrorCode::INVALID_PROPERTY, "Kode petak " + kode + " tidak dikenali!"){}
+};
+
+class GameStateException : public GameException{
+public:
+    GameStateException(const std::string& context) : GameException(GameErrorCode::GAME_STATE_ERROR, "Tidak diperbolehkan melakukan " + context + " sekarang"){}
 };
 
 #endif
