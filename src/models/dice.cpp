e72f9c8d4410ed12bba726a787e6c8ebcc0a4b00
arinazkk <arinaazkaaa@gmail.com>
2026-04-15 20:17:27 +0700

    feat: Card implementation template

--- src/models/dice.cpp
+++ src/models/dice.cpp
@@ -3,48 +3,61 @@
 
 Dice::Dice() : daduVal1(0), daduVal2(0), playerConcecutiveDoubles(0) {}
 
-void Dice::rollRandom() {
+void Dice::rollRandom()
+{
     // Menghasilkan angka acak antara 1 sampai 6
     daduVal1 = (rand() % 6) + 1;
     daduVal2 = (rand() % 6) + 1;
-    
+
     // Cek apakah dadu double
-    if (daduVal1 == daduVal2) {
+    if (daduVal1 == daduVal2)
+    {
         playerConcecutiveDoubles++;
-    } else {
+    }
+    else
+    {
         playerConcecutiveDoubles = 0;
     }
 }
 
-void Dice::setManual(int v1, int v2) {
+void Dice::setManual(int v1, int v2)
+{
     // Validasi exception rentang 1-6 dilakukan di AturDaduCommand sebelum memanggil ini
     daduVal1 = v1;
     daduVal2 = v2;
-    
+
     // Cek apakah dadu double
-    if (daduVal1 == daduVal2) {
+    if (daduVal1 == daduVal2)
+    {
         playerConcecutiveDoubles++;
-    } else {
+    }
+    else
+    {
         playerConcecutiveDoubles = 0;
     }
 }
 
-int Dice::getTotal() const {
+int Dice::getTotal() const
+{
     return daduVal1 + daduVal2;
 }
 
-int Dice::getConsecutiveDoubles() const {
+int Dice::getConsecutiveDoubles() const
+{
     return playerConcecutiveDoubles;
 }
 
-void Dice::resetConsecutiveDouvles() {
+void Dice::resetConsecutiveDouvles()
+{
     playerConcecutiveDoubles = 0;
 }
 
-int Dice::getDaduVal1() const {
+int Dice::getDaduVal1() const
+{
     return daduVal1;
 }
 
-int Dice::getDaduVal2() const {
+int Dice::getDaduVal2() const
+{
     return daduVal2;
 }
