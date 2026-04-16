2b19f5b9ee142e1765f2a21c934ced3bbed2b5bf
aufafaza <aufa.rienaldifaza.a@gmail.com>
2026-04-16 21:52:46 +0700

    feat: player class header definition

--- include/core/Player.hpp
+++ include/core/Player.hpp
@@ -1,12 +1,47 @@
 #pragma once 
-using namespace std; 
+
+#include <string> 
+#include <vector>
+using namespace std;
+
+
 namespace core {
-	class Player { 
-		private: 
-			int something_; 
-		public: 
-			Player(); 
-	}; 
+class Property; 
+class Game; 
+class Player { 
+	protected: 
+		string name_; 
+		string token_; 
+		int balance_; 
+		int position_; 
+		bool inJail_; 
+		bool isBankrupt_; 
+		int jailTurns_; 
+		vector<Property*> ownedProperties_;
+		//vector<ActionCard*> heldCards_; 
+	public: 
+		Player(string name); // assume that others will be inputted as defaults and there is no default names. 
+		virtual void takeTurn(Game&); 
+		virtual bool isHuman(); 
+		int getNetWorth();
+		bool canAfford(); // there's an int in the parameter but not stated apa isinya. 
+		void goToJail(); 
+		void releaseFromJail(); 
+		void addProperty(Property*); 
+		void removeProperty(Property*); 
+		bool operator+(Player&); 
+		bool operator-=(Player&);
+		bool operator<(Player&); 
+		bool operator>(Player&); 
+		bool operator==(Player&); 
+};
+
+class HumanPlayer : public Player {
+	public: 
+		void takeTurn(Game& g);
+		bool isHuman(); 
+};
+
 } 
 
 
