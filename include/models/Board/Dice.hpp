4bd002b9940d603828e9a410e2f125fb8365f793
unknown <naufal.saqib1@gmail.com>
2026-04-20 01:17:57 +0700

    feat: add GameEngine subclass

--- include/models/Board/Dice.hpp
+++ include/models/Board/Dice.hpp
@@ -1,2 +1,23 @@
 #pragma once
 
+#include <random>
+#include <utility>
+
+class Dice {
+private:
+    int die1;
+    int die2;
+    bool manualSet;
+    std::mt19937 rng;
+
+public:
+    Dice();
+
+    std::pair<int, int> roll();
+    void setManual(int d1, int d2);
+
+    int getDie1() const;
+    int getDie2() const;
+    int getTotal() const;
+    bool isDouble() const;
+};
