bef9af695b91ddf2346fe8c57f986e40e939ddd2
Fayyaz <fayyazakmallauda@gmail.com>
2026-04-21 07:36:06 +0700

    feat: implement CommunityChestCard

--- include/models/Card/CommunityChestCard.hpp
+++ include/models/Card/CommunityChestCard.hpp
@@ -1,2 +1,31 @@
 #pragma once
+#include <string>
+using namespace std;
 
+enum class CommunityChestType {
+    BIRTHDAY,   
+    DOCTOR,     
+    CAMPAIGN    
+};
+ 
+class CommunityChestCard {
+private:
+    CommunityChestType type;
+    std::string name;
+    std::string description;
+ 
+public:
+    CommunityChestCard(CommunityChestType type, const std::string& name, const std::string& desc);
+ 
+    CommunityChestCard();
+ 
+    ~CommunityChestCard();
+ 
+    void activate();
+ 
+    CommunityChestType getType() const;
+    std::string getName() const;
+    std::string getDescription() const;
+ 
+    bool operator==(const CommunityChestCard& other) const;
+};
