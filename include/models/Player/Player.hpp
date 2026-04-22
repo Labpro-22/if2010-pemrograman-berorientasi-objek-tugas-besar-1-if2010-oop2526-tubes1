10056969ea077c56270ac988500e7a8d5e40f7c4
staplesmaster <nicholasluischandra@gmail.com>
2026-04-22 23:00:46 +0700

    feat : add player additional getter setter function

--- include/models/Player/Player.hpp
+++ include/models/Player/Player.hpp
@@ -18,32 +18,38 @@ class Player
 {
 private:
     string username;
-    int balance;
-    int currentPosition;
-    PlayerStatus status;
-    int jailTurns;
-    int doubleCount;
+    int balance = 0;
+    int currentPosition = 0;
+    PlayerStatus status = ACTIVE;
+    int jailTurns = 0;
+    int doubleCount = 0;
     list<PropertyTile*> ownedProperties;
     vector<SkillCard*> skillCards;
 public:
+
+    Player() = default;
     void move(int steps);
+    void setName(string name);
+    void setBalance(int amount);
+    void setDoubleCount(int count);
     void setPosition(int pos);
-    
+    void setStatus(PlayerStatus status);
+    void setJailTurns(int currentJailAttempt);
 
     Player operator+(int amount); // add money
-    Player operator+=(int amount); // add money
+    Player& operator+=(int amount); // add money
     Player operator-(int amount); // reduce money
-    Player operator-=(int amount); // reduce money
+    Player& operator-=(int amount); // reduce money
     bool operator>=(int amount) const; // can afford suggested by me
     bool operator<(int amount) const; // can't afford
 
     bool canAfford(int amount) const;
     
-    bool operator>(Player &other) const; // compare richness
-    bool operator<(Player &other) const; // compare richness
-    bool operator==(Player &other) const; // compare richness
-    bool operator>=(Player &other) const; // compare richness
-    bool operator<=(Player &other) const; // compare richness
+    bool operator>(const Player &other) const; // compare richness
+    bool operator<(const Player &other) const; // compare richness
+    bool operator==(const Player &other) const; // compare richness
+    bool operator>=(const Player &other) const; // compare richness
+    bool operator<=(const Player &other) const; // compare richness
 
     void addProperty(PropertyTile* property);
     void removeProperty(PropertyTile* property);
@@ -51,12 +57,19 @@ public:
     void addSkillCard(SkillCard* card);
     void removeSkillCard(SkillCard* card);
 
-    
-    string getName();
-    int getBalance();
-    int getPosition();
-    PlayerStatus getStatus();
-    int getJailTurns();
+    SkillCard* getSkillCard(const string &cardName) const;
+    bool hasSkillCard(const SkillCard *card) const;
+    bool hasSkillCard(const string &cardName) const;
+    int getSkillCardCount() const;
+    bool canHoldMoreSkillCards() const;
+    string getName() const;
+    int getBalance() const;
+    int getPosition() const;
+    PlayerStatus getStatus() const;
+    int getJailTurns() const;
+
+    int totalWealth() const;
+
     int countOwnerRailroads() const; // Untuk railroad tapi perlu di bahas
     int countOwnerUtilities() const; // Untuk utility tapi perlu di bahas
 };
