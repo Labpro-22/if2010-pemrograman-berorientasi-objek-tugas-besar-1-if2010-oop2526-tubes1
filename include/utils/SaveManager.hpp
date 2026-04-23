9454f0b9e9fe5bfefd5f06bb10569d7abea8204e
DAN <87706283+ItsMeD4N@users.noreply.github.com>
2026-04-23 10:06:47 +0700

    feat(utils): implement SaveManager

---
+++ include/utils/SaveManager.hpp
@@ -0,0 +1,15 @@
+#ifndef SAVEMANAGER_HPP
+#define SAVEMANAGER_HPP
+
+#include <string>
+#include <vector>
+
+class GameManager;
+
+class SaveManager {
+public:
+    static bool saveGame(const GameManager& gm, const std::string& filename);
+    static bool loadGame(GameManager& gm, const std::string& filename);
+};
+
+#endif
