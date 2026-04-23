7cf0bed74ef5f79c8a4b67cdeedf9b266f745c8f
zekiamani1 <zekiamanioke@gmail.com>
2026-04-23 14:10:53 +0700

    add: board

--- include/models/Board.hpp
+++ include/models/Board.hpp
@@ -13,9 +13,11 @@ private:
     std::unordered_map<std::string, std::vector<Tile*>> colorTileMap;
 
 public:
+    Board();
     Tile& getTile(int index);
     void addTile(std::unique_ptr<Tile> tile);
     const std::vector<Tile*>& getPropertiesByGroup(const std::string& color) const;
     std::vector<std::string> getAllGroups() const;
     bool checkMonopoly(const Player& player, const std::string& color) const;
+    int getSize();
 };
