c73a8a824879f0f5f0592d0fac043163b3827c2d
billie-bytes <billiebaskarawibawa101@gmail.com>
2026-04-23 14:56:15 +0700

    implemented MoveActionCard and FinancialActionCard

--- include/models/StationActionCard.hpp
+++ include/models/StationActionCard.hpp
@@ -1,12 +1,8 @@
 #include "ActionCard.hpp"
 
 class StationActionCard : public ActionCard {
-    private:
-        Player& owner;
-        
-
+ 
     public:
-        StationActionCard(Player& owner);
         void onDraw(Player& p, Board& b, std::vector<Player>& all) override;
 
 
--- src/models/StationActionCard.cpp
+++ src/models/StationActionCard.cpp
@@ -3,12 +3,10 @@
 #include "Player.hpp"
 #include "PropertyTile.hpp"
 
-StationActionCard::StationActionCard(Player& owner):owner(owner){}
-
 void StationActionCard::onDraw(Player& p, Board& b, std::vector<Player>& all) {
     int current_pos = p.getPosition();
     
-    int total_tiles = 40; 
+    int total_tiles = b.getSize(); 
     for (int i = 1; i <= total_tiles; ++i) {
         int check_index = (current_pos + i) % total_tiles;
         Tile& ref = b.getTile(check_index);
