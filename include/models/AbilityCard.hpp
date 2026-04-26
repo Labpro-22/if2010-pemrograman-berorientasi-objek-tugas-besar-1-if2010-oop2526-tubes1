4f47d832d51c23a1fac7f03eafb71c74a30cf536
LutherCeltors <lutherceltors@gmail.com>
2026-04-26 18:39:12 +0700

    feat: add serialize method for every child of AbilityCard, and declare serialization as virtual method in AbilityCard.hpp

--- include/models/AbilityCard.hpp
+++ include/models/AbilityCard.hpp
@@ -10,6 +10,10 @@ class AbilityCard
 protected:
     std::string name;
     std::string description;
+    
+    static std::string escapeSerializedValue(const std::string& val);
+    static std::string serializeCommonAbilityFields(const std::string& name, const std::string& desc);
+
 public:
     AbilityCard(std::string name, std::string description) : name(name), description(description) {}
     virtual void setup() {}
@@ -17,6 +21,9 @@ public:
     virtual void use(Player* target, Game* game) = 0;
     std::string getName() const {
         return name;
+    }
+    std::string getDescription() const {
+        return description;
     }
     virtual std::string serialize() const;
     virtual ~AbilityCard() = default;
--- src/models/DerivedAbilityCard.cpp
+++ src/models/DerivedAbilityCard.cpp
@@ -1,5 +1,36 @@
 #include "../../include/models/DerivedAbilityCard.hpp"
 
+std::string AbilityCard::escapeSerializedValue(const std::string& value)
+{
+    std::string result;
+    result.reserve(value.size());
+
+    for (char c : value)
+    {
+        switch (c)
+        {
+            case '\\': result += "\\\\"; break;
+            case '|':  result += "\\|"; break;
+            case '=':  result += "\\="; break;
+            case '\n': result += "\\n"; break;
+            case '\r': result += "\\r"; break;
+            case '\t': result += "\\t"; break;
+            default:   result += c; break;
+        }
+    }
+
+    return result;
+}
+
+std::string AbilityCard::serializeCommonAbilityFields(
+    const std::string& name,
+    const std::string& description
+)
+{
+    return "NAME=" + escapeSerializedValue(name) +
+           "|DESCRIPTION=" + escapeSerializedValue(description);
+}
+
 MoveCard::MoveCard() : AbilityCard("MoveCard", "Maju x petak.") {}
 void MoveCard::setup() {
     std::mt19937 randomizer(std::random_device{}());
@@ -9,6 +40,11 @@ void MoveCard::setup() {
 }
 void MoveCard::use(Player* target, Game* game) {
     // TODO: MOVE PLAYER
+}
+
+std::string MoveCard::serialize() const 
+{
+    return serializeCommonAbilityFields(this->name, this->description) + "|STEPS=" + std::to_string(this->steps);
 }
 
 DiscountCard::DiscountCard() : AbilityCard("DiscountCard", "Dapatkan diskon selama 1 giliran.") {}
@@ -22,9 +58,19 @@ void DiscountCard::use(Player* target, Game* game) {
     // TODO: APPLY DISCOUNT
 }
 
+std::string DiscountCard::serialize() const
+{
+    return serializeCommonAbilityFields(this->name, this-> description) + "|PERCENTAGE=" + std::to_string(this->percentage);
+}
+
 ShieldCard::ShieldCard() : AbilityCard("ShieldCard", "Terlindung dari tagihan sewa maupun sanksi apapun yang merugikan selama 1 giliran.") {}
 void ShieldCard::use(Player* target, Game* game) {
     // TODO: APPLY SHIELD
+}
+
+std::string ShieldCard::serialize() const
+{
+    return serializeCommonAbilityFields(this->name, this-> description);
 }
 
 TeleportCard::TeleportCard() : AbilityCard("TeleportCard", "Bebas berpindah ke petak manapun di atas papan permainan.") {}
@@ -33,14 +79,28 @@ void TeleportCard::use(Player* target, Game* game) {
     // TODO: TELEPORT
 }
 
+std::string TeleportCard::serialize() const
+{
+    return serializeCommonAbilityFields(this->name, this-> description);
+}
+
 
 LassoCard::LassoCard() : AbilityCard("LassoCard", "Tarik satu pemain lain di depanmu ke posisimu.") {}
 void LassoCard::use(Player* target, Game* game) {
     // TODO: PULL OTHER PLAYER
 }
 
+std::string LassoCard::serialize() const
+{
+    return serializeCommonAbilityFields(this->name, this-> description);
+}
 
 DemolitionCard::DemolitionCard() : AbilityCard("DemolitionCard", "Hancurkan satu properti milik pemain lain.") {}
 void DemolitionCard::use(Player* target, Game* game) {
     // TODO: DEMOLISH
+}
+
+std::string DemolitionCard::serialize() const
+{
+    return serializeCommonAbilityFields(this->name, this-> description);
 }
